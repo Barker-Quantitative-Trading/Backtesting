b3d125bdc49085031d95fc580e9f58262c69249b
Kade McRae <kadejmcrae@outlook.com>
2025-09-30 20:30:40 -0700

    Added insert_candle functionality

--- src/data/candle.py
+++ src/data/candle.py
@@ -3,11 +3,16 @@
 candle.py
 Manages candle (price/time-series) data.
 """
+from src.data.db import execute_query, execute_update
 
 def insert_candle(symbol, timestamp, open_price, high, low, close, volume):
     """
     Insert candle data for a given symbol and timestamp.
     """
+    query="INSERT INTO candle (ts, open, high, low, close, volume) VALUES ('%s, %s, %s, %s, %s, %s');"
+    execute_query(query, (timestamp, open_price, high, low, close, volume))
+    query="INSERT INTO assets (symbol) VALUES ('%s');"
+    execute_query(query, (symbol))
     pass
 
 def get_candle(symbol, start_date, end_date):
