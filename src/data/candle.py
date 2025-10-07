546727be05e4b82fde68107cd3d9c0e1b752ae25
Kade McRae <kadejmcrae@outlook.com>
2025-10-07 13:18:16 -0700

    Fixes insert candle function

--- src/data/candle.py
+++ src/data/candle.py
@@ -5,14 +5,21 @@ Manages candle (price/time-series) data.
 """
 from src.data.db import execute_query, execute_update
 
-def insert_candle(symbol, timestamp, open_price, high, low, close, volume):
+def insert_candle(symbol, interval, timestamp, open_price, high, low, close, volume, adj_open, adj_high, adj_low, adj_close, adj_volume, div_cash, split_factor): #add the remaining columns from table
     """
     Insert candle data for a given symbol and timestamp.
     """
-    query="INSERT INTO candle (ts, open, high, low, close, volume) VALUES ('%s, %s, %s, %s, %s, %s');"
-    execute_query(query, (timestamp, open_price, high, low, close, volume))
-    query="INSERT INTO assets (symbol) VALUES ('%s');"
-    execute_query(query, (symbol))
+    query="""
+    SELECT id, asset_id, symbol
+    FROM candle
+    INNER JOIN assets ON candle.asset_id = assets.id;
+    """
+    execute_update(query)
+    query="""
+    INSERT INTO candle (symbol, interval, ts, open, high, low, close, volume, adj_open, adj_high, adj_low, adj_close, adj_volume, div_cash, split_factor) 
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);
+    """
+    execute_update(query, (symbol, interval, timestamp, open_price, high, low, close, volume, adj_open, adj_high, adj_low, adj_close, adj_volume, div_cash, split_factor))
     pass
 
 def get_candle(symbol, start_date, end_date):
